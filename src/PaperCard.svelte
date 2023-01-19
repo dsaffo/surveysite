efaf8f61e1f33db2fac0fbb51f8816ac644bdd4a
david saffo <saffo.d@husky.neu.edu>
2023-01-19 18:31:08 -0500

    now code

---
+++ src/PaperCard.svelte
@@ -0,0 +1,35 @@
+<script>
+     import Card, {
+    Content,
+    PrimaryAction,
+    Media,
+    MediaContent,
+  } from '@smui/card';
+</script>
+
+
+<div class="card-container">
+    <Card style="min-width: 300px;">
+      <Media class="card-media-square" aspectRatio="square">
+        <div style="color: #fff; position: absolute; bottom: 16px; left: 16px;">
+          <h2 class="mdc-typography--headline6" style="margin: 0;">
+            A card with square media.
+          </h2>
+          <h3 class="mdc-typography--subtitle2" style="margin: 0;">
+            And a subtitle.
+          </h3>
+        </div>
+      </Media>
+    </Card>
+  </div>
+
+
+<style>
+    * :global(.card-media-16x9) {
+      background-image: url(https://place-hold.it/320x180?text=16x9&fontsize=23);
+    }
+   
+    * :global(.card-media-square) {
+      background-image: url(https://place-hold.it/320x320?text=square&fontsize=23);
+    }
+  </style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,66 @@
+  
+<script>
+
+import List, { Group, Item, Subheader} from '@smui/list';
+import FilterGroup from '../FilterGroup.svelte';
+import PaperCard from '../PaperCard.svelte';
+
+$: innerHeight = 0;
+$: innerWidth = 0;
+
+</script>
+
+<svelte:window bind:innerHeight bind:innerWidth />
+
+<div class="container">
+    <div style="height: {innerHeight - 22}px; width: 20%;" class="demo-cell">
+        <Group>
+            <Subheader>Catagory 1</Subheader>
+            <List class="demo-list">
+                <FilterGroup></FilterGroup>
+            </List>
+          </Group>
+          <Group>
+            <Subheader>Catagory 2</Subheader>
+            <List class="demo-list">
+                <FilterGroup></FilterGroup>
+            </List>
+          </Group>
+    </div>
+
+    <div style="height: {innerHeight - 22}px; width: 55%;" class="demo-cell">
+        
+    </div>
+
+    <div style="height: {innerHeight - 22}px; width: 25%;" class="demo-cell">
+        <PaperCard></PaperCard>
+        <PaperCard></PaperCard>
+    </div>
+</div>
+
+
+
+<style>
+    .container {
+    display: flex; /* or inline-flex */
+    align-items: center;
+    }
+
+    .demo-cell {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 10px;
+      border: 1px;
+      border-style: solid;
+      border-color: var(--mdc-theme-secondary, #333);
+      color: var(--mdc-theme-secondary, rgb(15, 9, 9));
+    }
+
+    * :global(.demo-list) {
+    max-width: 600px;
+    border-left: 1px solid
+      var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));
+  }
+</style>
+  
